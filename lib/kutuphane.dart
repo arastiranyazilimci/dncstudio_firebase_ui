f3cd61d5505911a4b8c4cb94e84e0b06cbdb0945
Mustafa Uygur <mustafauygur@outlook.com>
2020-07-10 10:02:01 +0300

    Kutuphane Toast Colors Parametresi eklendi

--- lib/kutuphane.dart
+++ lib/kutuphane.dart
@@ -4,12 +4,12 @@ import 'package:flutter/material.dart';
 
 class kutuphane {
   static  FlutterToast flutterToast;
-  static void showToast(String mesaj,IconData ikon) {
+  static void showToast(String mesaj,IconData ikon,Color color) {
     Widget toast = Container(
       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(25.0),
-        color: Colors.greenAccent,
+        color: color,
       ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
